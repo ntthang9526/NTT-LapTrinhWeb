d07f8a57007d45526d6fb970f2bf9acc9b15bd76
victhor <ntthang9526@gmail.com>
2026-08-23 23:11:27 +0700

    Complete

--- Lesson2/MyWebsite/Controllers/HomeController.cs
+++ Lesson2/MyWebsite/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace MyWebsite.Controllers
         {
             return View();
         }
+        public IActionResult Product()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
